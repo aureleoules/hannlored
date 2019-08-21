599c4d252817902f387423d4b351dcd1a970f85a
unknown <aurele@oules.com>
2019-08-21 23:57:00 +0200

    display

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,24 @@
 import React from 'preact';
 
+import './styles.scss';
+import {Link} from 'preact-router';
 import axios from 'axios';
+
+import OnVisible from 'react-on-visible';
 
 class Home extends React.Component {
     
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            photos: [],
+            fetched: false
+        };
 
         axios.get('/photos').then(response => {
-            const photos = response.data.payload;
+            const photos = response.data.payload || [];
             console.log(photos);
+            this.setState({photos, fetched: true})
         }).catch(err => {
             if(err) throw err;
         });
@@ -19,8 +27,18 @@ class Home extends React.Component {
 
     render() {
         return (
-            <div>
-                Home
+            <div class="page home">
+                {this.state.fetched && <div class="photos-container">
+                    {this.state.photos.map((p, i) => (
+                        <OnVisible className="photo-container" visibleClassName={"slide-top"} percent={10}>
+                                <Link href={"/p/" + p.id}>
+                                    {p.title}
+                                </Link>
+                                <img src={p.url}/>
+                        </OnVisible>
+                        )
+                    )}
+                </div>}
             </div>
         );
     }
