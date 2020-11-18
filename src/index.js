8e230dcaed9251f89e2bb0f13422df69d0d0cae6
Aurèle Oulès <aurele@oules.com>
2020-11-18 21:02:27 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import axios from 'axios';
-axios.defaults.baseURL = 'https://backpulse.herokuapp.com/hannlore';
+axios.defaults.baseURL = 'https://backpulseapi.aureleoules.com/hannlore';
 
 React.render(<App />, document.getElementById('root'));
 
